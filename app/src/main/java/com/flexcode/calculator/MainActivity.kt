fe47dde25da05be62a7cc51281a14cc3ca8e131d
felix280murimi <felixmurimi280@gmail.com>
2021-06-09 20:10:16 +0300

    updating dependencies

--- app/src/main/java/com/flexcode/calculator/MainActivity.kt
+++ app/src/main/java/com/flexcode/calculator/MainActivity.kt
@@ -10,7 +10,9 @@ import net.objecthunter.exp4j.ExpressionBuilder
 
 
 
-class MainActivity : AppCompatActivity() {
+class
+
+MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
